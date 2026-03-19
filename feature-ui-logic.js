04027a2d991fce55e0b4a36af5081ff9c0211e80
Ruirong Xiang <rxiang25@ilb.cat>
2026-03-19 17:42:37 +0100

    Selectors creats

--- feature-ui-logic.js
+++ feature-ui-logic.js
@@ -1,2 +1,8 @@
 const MAX_RONDES = 5
 let rondesRestants = MAX_RONDES
+
+document.addEventListener('DOMContentLoaded', () => {
+    initSelectors()
+    setupEventListeners()
+    logTerminal('SYSTEM: Terminal inicialitzada. Tens 5 intents.', 'normal')
+})
